3a031b3792f7c0c0991ccbab804aa811885a8f0d
Aron <aronflodin@gmail.com>
2020-11-11 23:03:15 +0100

    Initial commit for months

---
+++ src/FluentDateTime/DateTime/Months.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Globalization;
+using FluentDate;
+
+
+namespace FluentDateTime
+{
+    public static class Months
+    {
+
+        public static DateTime January(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime January(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+
+        public static DateTime February(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime February(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime March(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime March(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime April(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime April(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime May(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime May(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime June(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime June(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime July(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime July(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime August(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime August(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+        public static DateTime September(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime September(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+
+        public static DateTime October(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime October(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+
+        public static DateTime November(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime November(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+
+        public static DateTime December(this int number)
+        {
+            return new DateTime(number, 1, 1);
+        }
+
+        public static DateTime December(this DateTime date)
+        {
+            return date.SetMonth(1);
+        }
+    }
+}
